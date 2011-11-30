0e16801acc8b0f3a50ab50cece32f23806470f24
Jason Byrne <jbyrne@milesplit.us>
2011-11-30 03:07:11 -0500

    moved subscriptions into modules, renamed some things, found more code shorthand and for loop speed-ups, added _initModule method, smaller overall

--- prereq.js
+++ prereq.js
@@ -19,8 +19,7 @@ var Prereq = (function(d) {
 	var Me = {},																	// self-reference
 		t=true,f=false,																// shortcuts for true/false
 		_last, 																		// to save name of last module in require
-		_scriptQ = {},																// queue of scripts
-		_subscriptions = {},														// pubsub subscriptions 
+		_modules = {},																// queue of scripts
 		_isArray = function(o) { return {}.toString.call(o) == '[object Array]' }, 	// test for array
 		_isFunction = function(o) { return (typeof o == 'function') }, 				// test for function
 		_isObject = function(o) { return o instanceof Object }, 					// test for function
@@ -33,18 +32,22 @@ var Prereq = (function(d) {
 		_sep = '_',																	// character to use as separator (saves a few bytes too)
 		// SMALL PUBSUB
 		_publish = function(name) {													// make sure subscription is initialized
-			_subscriptions[name] = _subscriptions[name] || [];						// get subscriptions for this module
-			_scriptQ[name].l = t;													// mark as loaded
-			for (var i=0; i < _subscriptions[name].length; i++) {					// loop through to all subscribers
-				_subscriptions[name][i](_scriptQ[name].e);							// hit the callback, with exports data if applicable
+			var module = _initModule(name);											// get module
+			module.l = t;															// mark as loaded
+			for (var i=0; i < module.s.length; i++) {								// loop through to all subscribers
+				module.s[i](module.e);												// hit the callback, with exports data if applicable
 			}
-			_subscriptions[name] = [];												// unsubscribe all
+			module.s = [];															// unsubscribe all
+		},
+		// get/initialize the module
+		_initModule = function(name){
+			return _modules[name] = _modules[name] || { l:f, e:{}, s:[] }			// get it and set it
 		},
 		// Add to queue
 		_qModule = function(name, path) {
-			if (!_scriptQ[name]) {													// if module does not yet exist in queue
-				_scriptQ[name] = { n:name, u:path, l:f, e:{} };						// n=name, u=array of urls, l=loaded?, e=exports (from module)
-				for (var i=0; i < path.length; i++) {								// Loop through array and add each
+			if (_modules[name]) {													// if module does not yet exist in queue
+				_initModule(name);
+				for (var i=0, len=path.length; i < len; i++) {						// Loop through array and add each
 					(path[i] != name) &&											// if group doesnt have same name as this one script
 						Me.subscribe(path[i], function(){							// listen for this to complete
 							Me.loaded(path) &&										// if all are loaded
@@ -56,15 +59,14 @@ var Prereq = (function(d) {
 		},
 		// include script and publish event once loaded
 		_includeScript = function(name, path) {
-			var path = (path || name),												// use name if path not set
-				path = _js.test(path) ? path : path + '.js',						// if no js at end then add it
+			_initModule(name);														// add it to queue
+			var path = _js.test(path) ? path : path + '.js',						// if no js at end then add it
 				path = (path.charAt(0) === '/' || path.match(/^\w+:/) ?				// is path an absolute path?
 					'' : _baseDir) + path;											// if not then prepend baseDir
 			var script = d.createElement('script');									// create script element
 			script.src = path;														// set src to path
 			script.async = t;														// load asynchronously
 			script.id = _sep + name;												// set an id so we can call it later
-			_scriptQ[name] = { n:name, u:path, l:f, e:{} };							// add it to queue
 			script.onload = script.onreadystatechange = function() {				// listen for changes to readyState
 				var rs = script.readyState;											// shortcut to readyState (saves a few bytes)
 				if (!rs || (/loaded|complete/).test(rs)) {							// is it done loading?
@@ -99,7 +101,7 @@ var Prereq = (function(d) {
 		}
 		var afterDepsLoaded = function(){											// will call this function once dependencies are loaded
 			if (Me.loaded(name)) {													// has this module already loaded?
-				callback(_scriptQ[name].e);											// yes, so hit the callback immediately
+				callback(_getModule(name).e);											// yes, so hit the callback immediately
 			} else {																// no, hasn't loaded yet...
 				Me.subscribe(name, callback);										// ... so subscribe to the pubsub event for when it loads
 				_qModule(name, path);												// and then put it in the queue to be loaded if it's not already
@@ -113,38 +115,32 @@ var Prereq = (function(d) {
 	};
 	// failover option
 	Me.failover = function(url, timeout) {
-		var module = _scriptQ[_last],												// recall last module referenced by require
-			timeout = (timeout > 0) ? timeout : 5000; 								// default timeout is 5 seconds
-		if (module) {																// did we find the last module?
+		timeout = timeout || 5000; 													// default timeout is 5 seconds
+		if (_modules[_last]) {														// did we find the last module?
 			var timer = setTimeout(function(){										// Set timer for when to go to failover
 				_head.removeChild(d.getElementById(_sep + _last));					// remove old script tag from head
 				_includeScript(_last, url);											// add new script tag
 			}, timeout);
-			Me.subscribe(_last, function(){											// listen for this module, in case it loads prior to timeout
+			return Me.subscribe(_last, function(){									// listen for this module, in case it loads prior to timeout
 				clearTimeout(timer);												// if it does then kill the timer
 			});
 		}
-		return Me;
 	};
 	// loaded?
-	Me.loaded = function(moduleNames) {												// Check on these modules/scripts to see if they have loaded
-		moduleNames = _arrayify(moduleNames);										// make moduleNames an array, if it's not already
-		for (var i=0; i < moduleNames.length; i++) {								// loop through each module
-			if (!(function(name) {													// has this specific one loaded?
-				if (!_scriptQ[name]) { return f; }									// if it's not even in the queue yet then the answer is no
-				else { return _scriptQ[name].l; }									// if it is in the queue then the answer is the value of the l property
-			})(moduleNames[i])) {													// ask above function about this specific module
-				return f;															// if any of them say they'd not loaded yet then return overall false
+	Me.loaded = function(names) {													// Check on these modules/scripts to see if they have loaded
+		names = _arrayify(names);													// make paths an array, if it's not already
+		for (var i=0, len=names.length;	i < len; i++) {								// loop through module names
+			if (!_initModule(names[i]).l) {											// if module is not loaded
+				return f;															// return false now
 			}
 		}
-		return (moduleNames.length == 0) ?											// were no arguments passed in?
-			_scriptQ :																// then they want a list of all modules and their status
+		return (len == 0) ?															// were no arguments passed in?
+			_modules :																// then they want a list of all modules and their status
 			t;																		// argument passed in and we made it this far... which means all were loaded
 	};
 	// Pubsub subscribe... decided to make public on Alan's suggestion, easy enough
 	Me.subscribe = function(name, callback) {
-		_subscriptions[name] = _subscriptions[name] || [];							// if no subscribers to this module yet, create the array
-		_subscriptions[name].push(callback);										// add this subscriber
+		_initModule(name).s.push(callback);											// add this subscriber
 		return Me;
 	};
 	// CommonJS type module definition
@@ -153,7 +149,7 @@ var Prereq = (function(d) {
 		c ?																			// If three arguments?
 			Me.require(b, function(){ c(exports); }) : 								// Make sure deps are loaded, then call the callback
 			b(exports); 															// No deps, so just hit the callback, passing in exports
-		_scriptQ[name].e = exports; 												// Store the output so that we can pass it to subscribers
+		_modules[name].e = exports; 												// Store the output so that we can pass it to subscribers
 		return Me;
 	};
 	return Me;																		// return self
